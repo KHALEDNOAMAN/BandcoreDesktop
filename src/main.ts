350acf562364de98dc20d157646fa42d6ea93edb
Nai64 <k3gn@proton.me>
2026-08-15 01:16:46 +0500

    redesign settings: sidebar sections, minimal layout

--- src/main.ts
+++ src/main.ts
@@ -340,8 +340,8 @@ function openSettings() {
         return;
     }
     settingsWindow = new BrowserWindow({
-        width: 460,
-        height: 560,
+        width: 640,
+        height: 620,
         parent: mainWindow,
         modal: false,
         resizable: false,
